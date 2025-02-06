ba9c012efe886e593a40bc52f8ad874e619da3b5
bariskbas <info@barisakbas.com.tr>
2025-02-06 11:49:07 +0300

    fixed

--- CORE/obj/Debug/net8.0/CORE.AssemblyInfo.cs
+++ CORE/obj/Debug/net8.0/CORE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CORE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380e26d39a5f76bf8d219bca37ea99d4e8c2fd8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03fb0889a6269fce8d9f4b97627a79b20183733")]
 [assembly: System.Reflection.AssemblyProductAttribute("CORE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CORE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
